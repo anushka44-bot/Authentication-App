3832b4e7eb4909c817f09740f1a97ba9dbf12f7b
Anushka Bag <bag82262@gmail.com>
2025-09-22 09:50:32 +0530

    Backend Complete

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from './routes/authRoutes.js';
+import userRouter from "./routes/userRoutes.js";
 
 const app=express();
 const port=process.env.PORT || 4000
@@ -18,5 +19,6 @@ app.use(cors({credentials:true}));
 
 app.get('/',(req,res)=> res.send("API Working"));
 app.use('/api/auth',authRouter);
+app.use('/api/user',userRouter);
 
 app.listen(port,()=>console.log(`Server listen on port:${port}`));
