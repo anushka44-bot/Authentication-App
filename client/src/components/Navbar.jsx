60a8a8901b9e50862af5f65d4b2443c9f6b4eb06
Anushka Bag <bag82262@gmail.com>
2025-09-25 21:27:07 +0530

    ..

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assets";
 
 function Navbar() {
   return (
-    <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 pr-10 absolute top-0">
+    <div className="w-full flex justify-between items-center ">
       <img src={assets.logo} alt="logo" className="w-28 sm:w-32" />
 
       <button className="mr-10 flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all">
