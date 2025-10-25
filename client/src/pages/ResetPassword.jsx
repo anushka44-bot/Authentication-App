57037070ca5064fcbb7442dbd0885c3fc51a12bf
Anushka Bag <bag82262@gmail.com>
2025-10-25 21:02:01 +0530

    Password reset form

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ResetPassword.css";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 function ResetPassword() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+
   return (
-    <div>
-      <h1>Reset Password</h1>
+    <div className="reset-password-container">
+      <img
+        onClick={() => navigate("/")}
+        src={assets.logo}
+        alt="logo"
+        className="reset-password-logo"
+      />
+
+      <form className="reset-password-form">
+        <h1 className="reset-password-title">Reset Password</h1>
+        <p className="reset-password-text">
+          Enter your registered email address
+        </p>
+
+        <div className="reset-password-input-wrapper">
+          <img
+            src={assets.mail_icon}
+            alt="mail"
+            className="reset-password-icon"
+          />
+          <input
+            type="email"
+            placeholder="Email id"
+            className="reset-password-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <button className="reset-password-btn">Submit</button>
+      </form>
     </div>
   );
 }
